bec2985f6cd2c982586ee5811aabca47a7d73373
Nasir <rahmanzadan@gmail.com>
2026-04-15 17:09:52 +0200

    Add AWS Lambda function configuration and zip packaging for bug tracker

---
+++ terraform/lambda.tf
@@ -0,0 +1,26 @@
+# Zip the Python code so Lambda can use it
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda/handler.py"
+  output_path = "${path.module}/../lambda/handler.zip"
+}
+
+# The Lambda function itself
+resource "aws_lambda_function" "bug_tracker" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "${var.project_name}-function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "handler.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.bugs.name
+    }
+  }
+
+  tags = {
+    Project = var.project_name
+  }
+}

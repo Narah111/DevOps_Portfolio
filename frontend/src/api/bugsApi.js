df6d168dad38d1e10e039310c60b7925b4537a57
Nasir <rahmanzadan@gmail.com>
2026-04-17 14:34:51 +0200

    Update API_URL to production endpoint for bug API

--- frontend/src/api/bugsApi.js
+++ frontend/src/api/bugsApi.js
@@ -1,5 +1,5 @@
 // ⚠️ Replace with your API Gateway URL after running terraform apply
-const API_URL = 'http://localhost:8000'
+const API_URL = 'https://xb2t4mn31i.execute-api.eu-north-1.amazonaws.com/prod'
 
 export const getAllBugs = async () => {
   const response = await fetch(`${API_URL}/bugs`)

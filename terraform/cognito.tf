6647e0848259c26790c55eec71e23d2c515a2b72
Nasir <rahmanzadan@gmail.com>
2026-04-19 17:02:58 +0200

    Refactor Cognito user pool configuration by removing comments and restructuring schema for name and family_name attributes

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -2,11 +2,9 @@
 resource "aws_cognito_user_pool" "main" {
   name = "${var.project_name}-user-pool"
 
-  # Users sign in with email
   username_attributes      = ["email"]
   auto_verified_attributes = ["email"]
 
-  # Password requirements
   password_policy {
     minimum_length    = 8
     require_lowercase = true
@@ -15,7 +13,6 @@ resource "aws_cognito_user_pool" "main" {
     require_uppercase = true
   }
 
-  # Email verification message
   verification_message_template {
     default_email_option = "CONFIRM_WITH_CODE"
     email_message        = "Your BugTracker verification code is {####}"
@@ -29,26 +26,38 @@ resource "aws_cognito_user_pool" "main" {
     mutable             = true
   }
 
+  schema {
+    attribute_data_type = "String"
+    name                = "name"
+    required            = true
+    mutable             = true
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name                = "family_name"
+    required            = true
+    mutable             = true
+  }
+
   tags = {
     Project = var.project_name
   }
 }
 
-# App Client - allows Lambda to communicate with Cognito
+# App Client
 resource "aws_cognito_user_pool_client" "main" {
   name         = "${var.project_name}-client"
   user_pool_id = aws_cognito_user_pool.main.id
 
-  # Allow Lambda to authenticate users with username/password
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
 
-  # Token expiry settings
-  access_token_validity  = 1   # 1 hour
-  id_token_validity      = 1   # 1 hour
-  refresh_token_validity = 30  # 30 days
+  access_token_validity  = 1
+  id_token_validity      = 1
+  refresh_token_validity = 30
 
   token_validity_units {
     access_token  = "hours"
@@ -56,6 +65,5 @@ resource "aws_cognito_user_pool_client" "main" {
     refresh_token = "days"
   }
 
-  # Do not return secret (not needed for Lambda auth flow)
   generate_secret = false
 }

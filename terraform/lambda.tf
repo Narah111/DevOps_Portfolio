1961490bfe0b5109a0bfc5c11214308d2e25ddca
Nasir <rahmanzadan@gmail.com>
2026-04-19 15:09:18 +0200

    Refactor Lambda configuration to zip entire source directory and update environment variables for Cognito integration

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,9 @@
 # Zip the Python code so Lambda can use it
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "${path.module}/../lambda/handler.py"
-  output_path = "${path.module}/../lambda/handler.zip"
+  source_dir  = "${path.module}/../lambda"
+  output_path = "${path.module}/../lambda.zip"
+  excludes    = ["lambda.zip", "__pycache__"]
 }
 
 # The Lambda function itself
@@ -16,7 +17,10 @@ resource "aws_lambda_function" "bug_tracker" {
 
   environment {
     variables = {
-      TABLE_NAME = aws_dynamodb_table.bugs.name
+      TABLE_NAME        = aws_dynamodb_table.bugs.name
+      USER_POOL_ID      = aws_cognito_user_pool.main.id
+      CLIENT_ID         = aws_cognito_user_pool_client.main.id
+      FRONTEND_URL      = "https://${aws_cloudfront_distribution.frontend.domain_name}"
     }
   }
 

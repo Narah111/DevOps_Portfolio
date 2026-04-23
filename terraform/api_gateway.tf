d7f9d65de7d468995e5d10f9520b33935450b9a9
Nasir <rahmanzadan@gmail.com>
2026-04-23 12:43:40 +0200

    Add /auth and /auth/{proxy+} resources with integration to Lambda

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -8,6 +8,20 @@ resource "aws_api_gateway_rest_api" "api" {
   }
 }
 
+# /auth resource
+resource "aws_api_gateway_resource" "auth" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "auth"
+}
+
+# /auth/{proxy+} - catches all auth routes
+resource "aws_api_gateway_resource" "auth_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.auth.id
+  path_part   = "{proxy+}"
+}
+
 # /bugs resource
 resource "aws_api_gateway_resource" "bugs" {
   rest_api_id = aws_api_gateway_rest_api.api.id
@@ -20,6 +34,14 @@ resource "aws_api_gateway_resource" "bug_id" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_resource.bugs.id
   path_part   = "{id}"
+}
+
+# Allow all HTTP methods on /auth/{proxy+}
+resource "aws_api_gateway_method" "auth_proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.auth_proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
 # Allow all HTTP methods on /bugs
@@ -36,6 +58,16 @@ resource "aws_api_gateway_method" "bug_id_method" {
   resource_id   = aws_api_gateway_resource.bug_id.id
   http_method   = "ANY"
   authorization = "NONE"
+}
+
+# Connect /auth/{proxy+} to Lambda
+resource "aws_api_gateway_integration" "auth_proxy_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.auth_proxy.id
+  http_method             = aws_api_gateway_method.auth_proxy_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.bug_tracker.invoke_arn
 }
 
 # Connect /bugs to Lambda
@@ -62,13 +94,30 @@ resource "aws_api_gateway_integration" "bug_id_integration" {
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
 
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.auth.id,
+      aws_api_gateway_resource.auth_proxy.id,
+      aws_api_gateway_resource.bugs.id,
+      aws_api_gateway_resource.bug_id.id,
+      aws_api_gateway_method.auth_proxy_method.id,
+      aws_api_gateway_method.bugs_method.id,
+      aws_api_gateway_method.bug_id_method.id,
+    ]))
+  }
+
   depends_on = [
+    aws_api_gateway_integration.auth_proxy_integration,
     aws_api_gateway_integration.bugs_integration,
     aws_api_gateway_integration.bug_id_integration
   ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-# Create a stage called "prod"
+# Create a stage "prod"
 resource "aws_api_gateway_stage" "prod" {
   deployment_id = aws_api_gateway_deployment.api_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api.id
